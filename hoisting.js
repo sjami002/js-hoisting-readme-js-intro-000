20219fea809aee6cfcd50b23a4b1879c16ff09d2
Shoptorshi Jamil <sjami002@gmail.com>
2019-01-21 21:19:34 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -21,12 +21,13 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
+  var name = "Kristin";
+  console.log(name);
+}
   var name = "Cricky";
-
-  sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    
   }
 }
